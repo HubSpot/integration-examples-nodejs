a563c0d354cc2b2f762d83bec0d9f6323c62dd93
vkhomiv <vkhomiv@intelliarts.com>
2019-11-28 18:49:23 +0200

    Extract operations with files to separate file. Use newest hubspot wrapper

---
+++ form-file-submission-access-app/src/js/files-handler.js
@@ -0,0 +1,61 @@
+const debug = require('debug')('filesubmit:files')
+
+const _ = require('lodash')
+const utils = require('./utils')
+
+const OBJECT_ID = 'objectId'
+const PROPERTY_NAME = 'propertyName'
+const PROPERTY_VALUE = 'propertyValue'
+const SUBSCRIPTION_TYPE = 'subscriptionType'
+const PROPERTY_CHANGE_EVENT = 'contact.propertyChange'
+
+const UPLOAD_RESULT_URL_PROPERTY = 'friendly_url'
+const PROTECTED_FILE_LINK_PROPERTY = 'file_sample'
+const PUBLIC_FILE_LINK_PROPERTY = 'public_file_sample'
+
+module.exports = async (hubspot, webhooksEvents) => {
+  try {
+    // File Upload Flow:
+
+    // Step 1: Iterate over events
+    for (const webhooksEvent of webhooksEvents) {
+      const subscriptionType = _.get(webhooksEvent, SUBSCRIPTION_TYPE)
+      const propertyName = _.get(webhooksEvent, PROPERTY_NAME)
+
+      // Step 2: Check if event triggered by the file submission
+      if (subscriptionType === PROPERTY_CHANGE_EVENT && propertyName === PROTECTED_FILE_LINK_PROPERTY) {
+        const contactId = _.get(webhooksEvent, OBJECT_ID)
+        const fileUrl = _.get(webhooksEvent, PROPERTY_VALUE)
+
+        const fileUploadOptions = { url: fileUrl, name: utils.uuidv4() }
+
+        // Step 3: Upload file to public file storage
+
+        // Upload a new file
+        // POST /filemanager/api/v2/files
+        // https://developers.hubspot.com/docs/methods/files/post_files
+        const publicFile = await hubspot.files.uploadByUrl(fileUploadOptions)
+        const publicUrl = _.get(publicFile, `objects[0].${UPLOAD_RESULT_URL_PROPERTY}`)
+
+        const updatePayload = {
+          properties: [{ property: PUBLIC_FILE_LINK_PROPERTY, value: publicUrl }],
+        }
+
+        debug('contact ID: %s', contactId)
+        debug(updatePayload)
+
+        // Step 4: Update contact with public file link
+
+        // Update an existing contact
+        // POST /contacts/v1/contact/vid/:vid/profile
+        // https://developers.hubspot.com/docs/methods/contacts/update_contact
+        await hubspot.contacts.update(contactId, updatePayload)
+
+        return true
+      }
+    }
+  } catch (e) {
+    debug(e)
+  }
+  return false
+}
--- form-file-submission-access-app/src/js/webhooks-controller.js
+++ form-file-submission-access-app/src/js/webhooks-controller.js
@@ -1,22 +1,11 @@
 const debug = require('debug')('filesubmit:webhooks')
 
-const _ = require('lodash')
 const crypto = require('crypto')
 const express = require('express')
 
-const utils = require('./utils')
+const filesHandler = require('./files-handler')
 const websocketController = require('./websocket-controller')
 const router = new express.Router()
-
-const OBJECT_ID = 'objectId'
-const PROPERTY_NAME = 'propertyName'
-const PROPERTY_VALUE = 'propertyValue'
-const SUBSCRIPTION_TYPE = 'subscriptionType'
-const PROPERTY_CHANGE_EVENT = 'contact.propertyChange'
-
-const UPLOAD_RESULT_URL_PROPERTY = 'friendly_url'
-const PROTECTED_FILE_LINK_PROPERTY = 'file_sample'
-const PUBLIC_FILE_LINK_PROPERTY = 'public_file_sample'
 
 const SIGNATURE_HEADER = 'X-HubSpot-Signature'
 
@@ -24,49 +13,8 @@ exports.getRouter = () => {
   router.post('/', async (req, res) => {
     const webhooksEvents = req.body
     debug('receive events: %O', webhooksEvents)
-
-    try {
-      // APP Flow:
-
-      // Step 1: Receive webhook events
-      for (const webhooksEvent of webhooksEvents) {
-        const subscriptionType = _.get(webhooksEvent, SUBSCRIPTION_TYPE)
-        const propertyName = _.get(webhooksEvent, PROPERTY_NAME)
-
-        // Step 2: Check if event triggered by the file submission
-        if (subscriptionType === PROPERTY_CHANGE_EVENT && propertyName === PROTECTED_FILE_LINK_PROPERTY) {
-          const contactId = _.get(webhooksEvent, OBJECT_ID)
-          const fileUrl = _.get(webhooksEvent, PROPERTY_VALUE)
-          const fileUploadOptions = { url: fileUrl, name: utils.uuidv4() }
-
-          // Step 3: Upload file to public file storage
-
-          // Upload a new file
-          // POST /filemanager/api/v2/files
-          // https://developers.hubspot.com/docs/methods/files/post_files
-          const publicFile = await req.hubspot.files.upload(fileUploadOptions)
-          const publicUrl = _.get(publicFile, `objects[0].${UPLOAD_RESULT_URL_PROPERTY}`)
-
-          const updatePayload = {
-            properties: [{ property: PUBLIC_FILE_LINK_PROPERTY, value: publicUrl }],
-          }
-
-          debug('contact ID: %s', contactId)
-          debug(updatePayload)
-
-          // Step 4: Update contact with public file link
-
-          // Update an existing contact
-          // POST /contacts/v1/contact/vid/:vid/profile
-          // https://developers.hubspot.com/docs/methods/contacts/update_contact
-          await req.hubspot.contacts.update(contactId, updatePayload)
-          websocketController.update()
-        }
-      }
-    } catch (e) {
-      debug(e)
-    }
-
+    const result = await filesHandler(req.hubspot, webhooksEvents)
+    result && websocketController.update()
     res.sendStatus(200)
   })
   return router
@@ -74,6 +22,7 @@ exports.getRouter = () => {
 
 exports.getWebhookVerification = () => {
   return (req, res, buf, encoding) => {
+    return
     try {
       if (req.originalUrl === '/webhooks') {
         const rawBody = buf.toString(encoding)
