301a8dd894db02ed3beee03b1b0211c4213a746d
vkhomiv <vkhomiv@hubspot.com>
2019-12-05 23:51:06 +0200

    add file uploading demo code

--- wrapper-file-upload-app/src/index.js
+++ wrapper-file-upload-app/src/index.js
@@ -1,52 +1,53 @@
-require('dotenv').config({path: '.env'});
+require('dotenv').config({ path: '.env' })
+const debug = require('debug')('file_upload:index')
 
-const _ = require('lodash');
-const path = require('path');
-const express = require('express');
-const Hubspot = require('hubspot');
-const bodyParser = require('body-parser');
+const fs = require('fs')
+const _ = require('lodash')
+const path = require('path')
+const express = require('express')
+const Hubspot = require('hubspot')
+const formidable = require('formidable')
+// const bodyParser = require('body-parser')
 
+const PORT = 3000
 
-const PORT = 3000;
-const CONTACTS_COUNT = 10;
+const CLIENT_ID = process.env.HUBSPOT_CLIENT_ID
+const CLIENT_SECRET = process.env.HUBSPOT_CLIENT_SECRET
+const SCOPES = 'contacts'
+const REDIRECT_URI = `http://localhost:${PORT}/oauth-callback`
+const UPLOAD_RESULT_URL_PROPERTY = 'friendly_url'
 
-const CLIENT_ID = process.env.HUBSPOT_CLIENT_ID;
-const CLIENT_SECRET = process.env.HUBSPOT_CLIENT_SECRET;
-const SCOPES = 'contacts';
-const REDIRECT_URI = `http://localhost:${PORT}/oauth-callback`;
+let fileByUrl
+let fileFromComputer
 
-let tokenStore = {};
+let tokenStore = {}
 
 const checkEnv = (req, res, next) => {
-  if (_.startsWith(req.url, '/error')) return next();
+  if (_.startsWith(req.url, '/error')) return next()
 
-  if (_.isNil(CLIENT_ID)) return res.redirect('/error?msg=Please set HUBSPOT_CLIENT_ID env variable to proceed');
-  if (_.isNil(CLIENT_SECRET)) return res.redirect('/error?msg=Please set HUBSPOT_CLIENT_SECRET env variable to proceed');
+  if (_.isNil(CLIENT_ID)) return res.redirect('/error?msg=Please set HUBSPOT_CLIENT_ID env variable to proceed')
+  if (_.isNil(CLIENT_SECRET)) return res.redirect('/error?msg=Please set HUBSPOT_CLIENT_SECRET env variable to proceed')
 
-  next();
-};
+  next()
+}
 
 const isAuthorized = () => {
-  return !_.isEmpty(tokenStore.refresh_token);
-};
+  return !_.isEmpty(tokenStore.refresh_token)
+}
 
 const isTokenExpired = () => {
-  return Date.now() >= tokenStore.updated_at + tokenStore.expires_in * 1000;
-};
+  return Date.now() >= tokenStore.updated_at + tokenStore.expires_in * 1000
+}
 
-const prepareContactsContent = (contacts) => {
-  return _.map(contacts, (contact) => {
-    const companyName = _.get(contact, 'properties.company.value') || '';
-    const name = getFullName(contact.properties);
-    return {vid: contact.vid, name, companyName};
-  });
-};
+const fileToBuffer = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file.path, (err, data) => {
+      if (err) return reject(err)
 
-const getFullName = (contactProperties) => {
-  const firstName = _.get(contactProperties, 'firstname.value') || '';
-  const lastName = _.get(contactProperties, 'lastname.value') || '';
-  return `${firstName} ${lastName}`
-};
+      resolve(data)
+    })
+  })
+}
 
 const refreshToken = async () => {
   hubspot = new Hubspot({
@@ -54,109 +55,132 @@ const refreshToken = async () => {
     clientSecret: CLIENT_SECRET,
     redirectUri: REDIRECT_URI,
     scopes: SCOPES,
-    refreshToken: tokenStore.refresh_token
-  });
+    refreshToken: tokenStore.refresh_token,
+  })
 
-  tokenStore = await hubspot.refreshAccessToken();
-  tokenStore.updated_at = Date.now();
-  console.log('Updated tokens', tokenStore);
-};
+  tokenStore = await hubspot.refreshAccessToken()
+  tokenStore.updated_at = Date.now()
+  debug('Updated tokens', tokenStore)
+}
 
-
-const app = express();
+const app = express()
 
 let hubspot = new Hubspot({
   clientId: CLIENT_ID,
   clientSecret: CLIENT_SECRET,
   redirectUri: REDIRECT_URI,
   scopes: SCOPES,
-});
+})
 
-app.use(express.static('public'));
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'))
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 
-app.use(bodyParser.urlencoded({
-  limit: '50mb',
-  extended: true,
-}));
+// app.use(
+//   bodyParser.urlencoded({
+//     limit: '50mb',
+//     extended: true,
+//   })
+// )
+//
+// app.use(
+//   bodyParser.json({
+//     limit: '50mb',
+//     extended: true,
+//   })
+// )
 
-app.use(bodyParser.json({
-  limit: '50mb',
-  extended: true,
-}));
+app.use((req, res, next) => {
+  debug(req.method, req.url)
+  next()
+})
 
-app.use(checkEnv);
+app.use(checkEnv)
 
 app.get('/', async (req, res) => {
   try {
-    if (!isAuthorized()) return res.render('login');
-    if (isTokenExpired()) await refreshToken();
-
-    // Get all contacts
-    // GET /contacts/v1/lists/all/contacts/all
-    // https://developers.hubspot.com/docs/methods/contacts/get_contacts
-    console.log('Calling contacts.get API method. Retrieve all contacts.');
-    const contactsResponse = await hubspot.contacts.get({count: CONTACTS_COUNT});
-    console.log('Response from API', contactsResponse);
-
-    res.render('contacts', { tokenStore, contacts: prepareContactsContent(contactsResponse.contacts) });
+    if (!isAuthorized()) return res.render('login')
+    if (isTokenExpired()) await refreshToken()
+    res.render('dashboard', { fileByUrl: fileByUrl || '', fileFromComputer: fileFromComputer })
   } catch (e) {
-    console.error(e);
-    res.redirect(`/error?msg=${e.message}`);
+    console.error(e)
+    res.redirect(`/error?msg=${e.message}`)
   }
-});
+})
 
 app.use('/oauth', async (req, res) => {
-
   const authorizationUrlParams = {
     client_id: CLIENT_ID,
     redirect_uri: REDIRECT_URI,
-    scopes: SCOPES
-  };
+    scopes: SCOPES,
+  }
 
   // Use the client to get authorization Url
   // https://www.npmjs.com/package/hubspot
-  console.log('Creating authorization Url');
-  const authorizationUrl = hubspot.oauth.getAuthorizationUrl(authorizationUrlParams);
-  console.log('Authorization Url', authorizationUrl);
+  debug('Creating authorization Url')
+  const authorizationUrl = hubspot.oauth.getAuthorizationUrl(authorizationUrlParams)
+  debug('Authorization Url', authorizationUrl)
 
-  res.redirect(authorizationUrl);
-});
+  res.redirect(authorizationUrl)
+})
 
 app.use('/oauth-callback', async (req, res) => {
-  const code = _.get(req, 'query.code');
+  const code = _.get(req, 'query.code')
 
   // Get OAuth 2.0 Access Token and Refresh Tokens
   // POST /oauth/v1/token
   // https://developers.hubspot.com/docs/methods/oauth2/get-access-and-refresh-tokens
-  console.log('Retrieving access token by code:', code);
-  tokenStore = await hubspot.oauth.getAccessToken({code});
-  console.log('Retrieving access token result:', tokenStore);
-  tokenStore.updated_at = Date.now();
+  debug('Retrieving access token by code:', code)
+  tokenStore = await hubspot.oauth.getAccessToken({ code })
+  debug('Retrieving access token result:', tokenStore)
+  tokenStore.updated_at = Date.now()
 
   // Set token for the
   // https://www.npmjs.com/package/hubspot
-  hubspot.setAccessToken((tokenStore.access_token));
-  res.redirect('/');
-});
+  hubspot.setAccessToken(tokenStore.access_token)
+  res.redirect('/')
+})
 
 app.get('/login', (req, res) => {
-  tokenStore = {};
-  res.redirect('/');
-});
+  tokenStore = {}
+  res.redirect('/')
+})
 
-app.get('/refresh', async (req, res) => {
-  if (isAuthorized()) await refreshToken();
-  res.redirect('/');
-});
+app.get('/upload', async (req, res) => {
+  const url = _.get(req, 'query.url')
+
+  if (url) {
+    debug('uploading file by URL ', url)
+    const uploadingResult = await hubspot.files.uploadByUrl({ url, name: `${Date.now()}` })
+    fileByUrl = _.get(uploadingResult, `objects[0].${UPLOAD_RESULT_URL_PROPERTY}`)
+  }
+  res.redirect('/')
+})
+
+app.post('/upload', async (req, res) => {
+  try {
+    new formidable.IncomingForm().parse(req, async (err, fields, files) => {
+      if (err) throw err
+
+      const name = _.get(files, 'content.name')
+
+      const content = await fileToBuffer(files.content)
+      const uploadingResult = await hubspot.files.upload({ content, name })
+      fileFromComputer = _.get(uploadingResult, `objects[0].${UPLOAD_RESULT_URL_PROPERTY}`)
+
+      res.redirect('/')
+    })
+  } catch (e) {
+    debug(e)
+  }
+})
 
 app.get('/error', (req, res) => {
-  res.render('error', {error: req.query.msg});
-});
+  res.render('error', { error: req.query.msg })
+})
 
 app.use((error, req, res, next) => {
-  res.render('error', {error: error.message});
-});
+  res.render('error', { error: error.message })
+})
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+app.listen(PORT, () => debug(`Listening on http://localhost:${PORT}`))
